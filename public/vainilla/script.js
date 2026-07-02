7cae28557d21e4a21549eaad0be9a775eb6e629f
JuanAgustinBarreto <j_a_barreto@hotmail.com>
2026-07-02 17:12:35 -0300

    Primer commit portfolio

--- public/vainilla/script.js
+++ public/vainilla/script.js
@@ -140,7 +140,7 @@ const projects = [
     image: "img/project-1.jpg",
     tags: ["HTML", "CSS", "JavaScript"],
     github: GITHUB,
-    demo: GITHUB,
+    demo: "https://hekel.vercel.app/",
   },
   {
     name: "Bien Criollo Nogoyá",
@@ -148,7 +148,7 @@ const projects = [
     image: "img/project-2.jpg",
     tags: ["HTML", "CSS", "JavaScript"],
     github: GITHUB,
-    demo: GITHUB,
+    demo: "https://biencriollo.vercel.app/",
   },
   {
     name: "Marcelo Barreto Propiedades - Inmobiliaria Nogoyá",
@@ -156,7 +156,7 @@ const projects = [
     image: "img/project-3.jpg",
     tags: ["HTML", "CSS", "JavaScript"],
     github: GITHUB,
-    demo: GITHUB,
+    demo: "https://marcelobarretopropiedades.vercel.app/",
   },
   {
     name: "Detalles que Enamoran",
@@ -164,7 +164,7 @@ const projects = [
     image: "img/project-4.jpg",
     tags: ["HTML", "CSS", "JavaScript"],
     github: GITHUB,
-    demo: GITHUB,
+    demo: "https://detallesqueenamoran.vercel.app/",
   },
   {
     name: "Bien Criollo Nogoyá - Gestión",
@@ -174,7 +174,7 @@ const projects = [
     github: GITHUB,
     demo: GITHUB,
   },
-   {
+  {
     name: "Marcelo Barreto Propiedades Nogoyá - Gestión",
     description: "Sistema de Gestión de propiedades, clientes, reportes entre fechas, día, semana, mes, año, exportaciones a excel, pdf y gráficos de propiedades y clientes. Desarrollado con HTML, CSS, JavaScript y Node.js.",
     image: "img/project-6.jpg",
@@ -188,9 +188,10 @@ const projects = [
     image: "img/project-7.jpg",
     tags: ["HTML", "CSS", "JavaScript", "JSON"],
     github: GITHUB,
-    demo: GITHUB,
+    demo: "https://meier-distribuciones.vercel.app/",
   },
 ];
+
 
 const iconGithub =
   '<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/><path d="M9 18c-4.51 2-5-2-7-2"/></svg>';
